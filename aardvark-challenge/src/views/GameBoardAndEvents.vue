75e1398754b7a310e02432eeae324d8931d63568
EvaldasL <evaldas@pendulumcreative.uk>
2020-10-28 11:11:33 +0200

    - Updated constancts file with new routes - Added initial countdown

--- aardvark-challenge/src/views/GameBoardAndEvents.vue
+++ aardvark-challenge/src/views/GameBoardAndEvents.vue
@@ -1,18 +1,25 @@
 <template>
   <div>
-    {{slots}}
-    <div>{{apiUrl}}</div>
     <ul class="wheel">
-      <li v-for="(value, index) in positionToId" :key="index" class="wheel-item" :style="{transform: `rotateZ(${360/slots*index}deg)`}">
-        <span class="wheel-number">{{value}}</span>
+      <li
+        v-for="(position, index) in positionToId"
+        :key="index" class="wheel-item"
+        :style="{transform: `rotateZ(${slots === 0 ? 0: 360 / slots * index}deg)`}"
+      >
+        <span class="wheel-number">{{position}}</span>
       </li>
     </ul>
+    <div class="timer">
+      <h2>Countdown until the next game</h2>
+      {{countDownValue ? countDownValue : 'not counting' }}
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator'
 import axios from 'axios'
+import { NEXT_GAME_PATH, WHEEL_CONFIG_PATH } from '@/constants'
 
 @Component({
 
@@ -23,20 +30,48 @@ export default class Home extends Vue {
   public name = '';
   public positionToId = [];
   public results = [];
-  public slots = 1;
+  public slots = 0;
+
+  public countDownIntervalId;
+  public countDownValue = 0;
 
   mounted () {
     axios
-      .get('https://dev-games-backend.advbet.com/v1/ab-roulette/1/configuration')
+      .get(`${this.apiUrl}${WHEEL_CONFIG_PATH}`)
       .then(response => {
         const data = response.data
         this.slots = data.slots
         this.positionToId = data.positionToId
+
+        this.getNextGame(this.initCountdown)
       // public colors = [];
       // public name = '';
       // public positionToId = '';
       // public results = [];
       })
+    // SPIN_BY_UUID_PATH
+  }
+
+  getNextGame (callback) {
+    axios
+      .get(`${this.apiUrl}${NEXT_GAME_PATH}`)
+      .then(response => callback(response.data))
+  }
+
+  initCountdown (data) {
+    this.countDown(data.fakeStartDelta)
+  }
+
+  countDown (seconds) {
+    this.countDownValue = seconds
+
+    this.countDownIntervalId = setInterval(() => {
+      this.countDownValue--
+
+      if (this.countDownValue === 0) {
+        clearInterval(this.countDownIntervalId)
+      }
+    }, 1000)
   }
 
   updated () {
@@ -54,7 +89,6 @@ export default class Home extends Vue {
     padding: 0;
     list-style-type: none;
   }
-
   .wheel-item {
     position: absolute;
     list-style-type: none;
@@ -86,5 +120,8 @@ export default class Home extends Vue {
     position: absolute;
     top: -175px;
     left: -16px;
+  }
+  .timer {
+    font-size: 32px;
   }
 </style>
